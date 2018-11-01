3c86f3183f170063efbbf2ca662efc3fa2355dd0
icland01 <862912149@qq.com>
2018-11-01 18:40:37 +0800

    模型的使用

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,4 +1,6 @@
+import datetime
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class Question(models.Model):
@@ -8,13 +10,21 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
 
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
 class Choice(models.Model):
     question = models.ForeignKey(Question,on_delete=models.CASCADE,)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
 
+    class Meta:
+        db_table = 'db_choice'
+
     def __str__(self):
         return self.choice_text
 
     def was_published_recently(self):
-    	return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
